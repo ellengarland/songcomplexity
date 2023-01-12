401f2a34af516fe26252b9d85d9f794fc1ddbae8
Ellen Garland <ellen.garland@gmail.com>
2023-01-12 12:07:38 +0000

    added theme counts coding

--- ComplexityCounts.py
+++ ComplexityCounts.py
@@ -5,7 +5,8 @@ file = open(sys.argv[1], "r")
 lines = file.read().splitlines()[1:]
 
 # Initially there are no groups
-groups = {}
+unit_groups = {}
+theme_groups = {}
 
 # Process each line
 for line in lines:
@@ -17,18 +18,36 @@ for line in lines:
     theme = cells[3]
     units = cells[4:]
     units = [u for u in units if u != '']
-    if (song_file, cycle) in groups:
+    if (song_file, cycle) in unit_groups:
         # If the (song_file, cycle) tuple has been seen before, add the units to it
-        groups[(song_file, cycle)].extend(units)
+        unit_groups[(song_file, cycle)].extend(units)
     else:
         # Otherwise, create a new entry in the map of groups
-        groups[(song_file, cycle)] = units
+        unit_groups[(song_file, cycle)] = units
+
+    if (song_file, cycle) in theme_groups:
+        # If the (song_file, cycle) tuple has been seen before, add the theme to it
+        theme_groups[(song_file, cycle)].extend([theme])
+    else:
+        # Otherwise, create a new entry in the map of theme groups
+        theme_groups[(song_file, cycle)] = [theme]
+
+
 
 # Print out the result
-for (song_file, cycle) in groups:
+for (song_file, cycle) in unit_groups:
     # Get rid of duplicates here
-    units = groups[(song_file, cycle)]
+    units = unit_groups[(song_file, cycle)]
     unique_units = list(set(units))
-    # Print the output
+    # Print the output which is: song file, song cycle, total #units in song cycle, #unique units in song cycle, list of unique units.  
     #print(f"{song_file} {cycle} has {len(units)} units in total, of which {len(unique_units)} are unique units: {unique_units}")
     print(f"{song_file},{cycle},{len(units)},{len(unique_units)},{','.join(unique_units)}")
+
+print()
+
+for (song_file, cycle) in theme_groups:
+    # Get rid of duplicates here
+    themes = theme_groups[(song_file, cycle)]
+    unique_themes = list(set(themes))
+    # Print the output which is: song file, song cycle, #unique themes in song cycle, list of unique themes.  
+    print(f"{song_file},{cycle},{len(unique_themes)},{','.join(unique_themes)}")
